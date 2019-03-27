c18cecdcb6cf378bcf1845d07877805f965d9814
Larion <larion.turlo@gmail.com>
2019-03-27 08:59:48 +0400

    Change export file hydration

--- hydrators/index.js
+++ hydrators/index.js
@@ -1,12 +1,17 @@
+const apiConst = require('../apiConst');
 
 const stashDocsFunction = (z, bundle) => {
     // use standard auth to request the file
-const filePromise = z.request({
-    url: `https://${bundle.authData.api_server}${apiConst.routes.createExport}/${bundle.inputData.exportId}`,
-    raw: true
-});
-// and swap it for a stashed URL
-return z.stashFile(filePromise);
+    const filePromise = z.request({
+        url: `https://${bundle.authData.api_server}${apiConst.routes.createExport}/${bundle.inputData.exportId}`,
+        raw: true
+    });
+    // and swap it for a stashed URL
+    return z.stashFile(filePromise)
+        .then((url) => {
+            z.console.log(`Stashed URL = ${url}`);
+            return url;
+        });
 };
 
 module.exports = {
