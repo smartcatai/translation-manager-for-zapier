f0cf03fa750300adb76c988eeb0e3dc0e2f05b14
Larion <larion.turlo@gmail.com>
2019-08-02 08:33:03 +0400

    Removed trigger by statistic calculated

--- triggers/index.js
+++ triggers/index.js
@@ -1,12 +1,10 @@
 const canceledProject = require('./canceledProject');
 const completedProject = require('./completedProject');
 const newProject = require('./newProject');
-const statisticProject = require('./statisticProject');
 
 
 module.exports = {
     [canceledProject.key]: canceledProject,
     [completedProject.key]: completedProject,
     [newProject.key]: newProject,
-    [statisticProject.key]: statisticProject,
 }
